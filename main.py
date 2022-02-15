522cb5bd62c219364ec55dfe4c7e77640bfe0051
MD211211 <66662407+MD211211@users.noreply.github.com>
2022-02-15 08:09:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>https://mineton.pro</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>welcome to linkki.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
