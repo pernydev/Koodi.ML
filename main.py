9c087ea3e019fe4559cddf963774c7b40fba5d16
MD211211 <66662407+MD211211@users.noreply.github.com>
2022-02-15 08:18:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://mineton.pro</title></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>https://linkki.ml</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>welcome to linkki.</p>", "utf-8"))
