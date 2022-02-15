e2e30190be43ca2d79f3944494a076575bdb753f
MD211211 <66662407+MD211211@users.noreply.github.com>
2022-02-15 07:54:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = "40003"
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
