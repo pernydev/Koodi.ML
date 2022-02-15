3484206d671d4ed3858ae420be03bc4b7ce6dd69
pernydev <pernydev@gmail.com>
2022-02-15 14:50:33 +0200

    Added the URL to the page (debug)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
-code = dict()
 
 hostName = "0.0.0.0"
 serverPort = 40003
+code = dict()
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -13,7 +13,8 @@ class MyServer(BaseHTTPRequestHandler):
         if self.path == "/":
             self.wfile.write(bytes("<p>front</p>", "utf-8"))
         else:
-            self.wfile.write(bytes("<p>created</p>", "utf-8"))
+            url = self.path[1:]
+            self.wfile.write(bytes("<p>created code to"+url+"</p>", "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
